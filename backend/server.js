d6670a3d68c1a5d690198c05b2c0a64b0cd4f630
Pithani Venkata Durga Sri Satya <112044322+18048Satya@users.noreply.github.com>
2025-03-28 18:34:40 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-origin: process.env.PORT || 'http://localhost:5173',
+origin: process.env.PORT || 'https://letter-web-app-f.onrender.com',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
